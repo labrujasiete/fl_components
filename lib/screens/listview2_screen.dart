1dff818559247251c4cf6f53745d448fd65074e3
laBruja-Team <labrujasiete@gmail.com>
2023-02-10 00:24:24 -0800

    2-10-23

---
+++ lib/screens/listview2_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Listview2Screen extends StatelessWidget {
+   
+  final options = const['megaman', 'metal gear', 'super smash', 'final fantasy'];
+
+  const Listview2Screen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("ListView"),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView.separated(
+        itemCount: options.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(options[index]),
+          trailing: const Icon(Icons.arrow_forward_ios, color: Colors.indigo),
+          onTap: () => {},
+        ), 
+        separatorBuilder: (_, __) => const Divider(), 
+        ),
+    );
+  }
+}
